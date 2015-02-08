df1d70a98c87640b90d1e4fcbea663df4f239e81
Phase <ph4sesaber@gmail.com>
2015-02-07 23:54:27 -0800

    Initial Commit

---
+++ src/xyz/jadonfowler/brutalpixel/game/Game.java
@@ -0,0 +1,12 @@
+/**
+ * BrutalPixel was made by Jadon Fowler.
+ * This file is licensed under the MIT License.
+ */
+package xyz.jadonfowler.brutalpixel.game;
+
+/**
+ * @author Jadon "Phase" Fowler on Feb 7, 2015
+ */
+public class Game {
+
+}
---
+++ src/xyz/jadonfowler/brutalpixel/launch/Launcher.java
@@ -0,0 +1,20 @@
+/**
+ * BrutalPixel was made by Jadon Fowler.
+ * This file is licensed under the MIT License.
+ */
+package xyz.jadonfowler.brutalpixel.launch;
+
+/**
+ * @author Jadon "Phase" Fowler on Feb 7, 2015
+ */
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
